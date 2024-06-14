205b09e30962a159c820f154f39dee2870fc4a96
ChGSufro <c.gajardo08@ufromail.cl>
2024-06-14 12:26:40 -0400

    Credenciales base de datos

---
+++ src/main/java/sql/SQL.java
@@ -0,0 +1,10 @@
+package sql;
+
+public class SQL {
+
+        private String url = "jdbc:postgresql://base-de-datos.cwl2z8i6c6vy.us-east-1.rds.amazonaws.com";
+        private String user = "postgres";
+        private String password = "C#20gc15";
+
+
+}
