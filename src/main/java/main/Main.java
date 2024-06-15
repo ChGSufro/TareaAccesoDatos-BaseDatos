d83d037e92d6be011ed0060a45f40a465d0c8bfb
ChGSufro <c.gajardo08@ufromail.cl>
2024-06-15 16:00:52 -0400

    test select

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,21 @@
 package main;
+import sql.SQL;
+
+import javax.xml.transform.Result;
+import java.sql.ResultSet;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SQL sql = new SQL();
+        try {
+            ResultSet datos = sql.get_datos_animal();
+            while (datos.next()){
+                System.out.println(datos.getString("nombre"));
+            }
+        } catch (Exception e) {
+            System.out.println("Error al obtener los datos");
+            e.printStackTrace();
+        }
+
     }
 }
--- src/main/java/sql/SQL.java
+++ src/main/java/sql/SQL.java
@@ -1,10 +1,41 @@
 package sql;
 
+import java.sql.*;
+import java.util.AbstractList;
+
 public class SQL {
 
-        private String url = "jdbc:postgresql://base-de-datos.cwl2z8i6c6vy.us-east-1.rds.amazonaws.com";
+        private final String url = "jdbc:postgresql://base-de-datos.cwl2z8i6c6vy.us-east-1.rds.amazonaws.com/CanesFelinos";
         private String user = "postgres";
         private String password = "C#20gc15";
+        private Connection conexion;
+        private Statement statement;
+
+    public SQL(){
+        try {
+            conexion = conectar_base_datos();
+            statement = conexion.createStatement();
+        } catch (SQLException e) {
+            System.out.println("Error al conectar a la base de datos");
+            e.printStackTrace();
+        }
+    }
+
+        private Connection conectar_base_datos() throws SQLException{
+            Connection conexion = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexión exitosa");
+            return conexion;
+        }
+
+        private void desconectar_base_datos() throws SQLException{
+            conexion.close();
+            System.out.println("Desconexión exitosa");
+        }
+
+        public ResultSet get_datos_animal() throws SQLException{
+                ResultSet datos = statement.executeQuery("SELECT * FROM animal");
+                return datos;
+        }
 
 
 }
