cef01b7ca07fb3fcab5e48a488e7bad69738fb3b
ChGSufro <c.gajardo08@ufromail.cl>
2024-06-24 17:43:35 -0400

    merge master

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,10 +7,15 @@ public class Main {
     public static void main(String[] args) {
         SQL sql = new SQL();
         Menu menu = new Menu(sql);
+
         try {
+
             menu.iniciar();
+
         } catch (RuntimeException error){
+
             System.out.println(error.getMessage());
+
         }
     }
 }
